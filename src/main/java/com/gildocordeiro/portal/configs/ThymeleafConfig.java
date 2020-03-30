b40465bfe1eb7a96ad4ad394709deb15efee5d41
gildo-cordeiro <gildo.duartec@gmail.com>
2020-03-30 14:12:48 -0300

    Modificações na configuração do Thymeleaf.

--- src/main/java/com/gildocordeiro/portal/configs/ThymeleafConfig.java
+++ src/main/java/com/gildocordeiro/portal/configs/ThymeleafConfig.java
@@ -1,59 +1,90 @@
 package com.gildocordeiro.portal.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
 
 //Arquivo de configuração do Thymeleaf
-//Sobre Beans: https://www.baeldung.com/spring-bean
 @Configuration
-public class ThymeleafConfig {
+public class ThymeleafConfig extends WebMvcConfigurationSupport {
 
-	//Injeção de dependencias atraves da anotação Autowired
-	//Sobre Autowired: https://www.baeldung.com/?s=Autowired
-	@Autowired
-	private ApplicationContext applicationContext;
-	
+	private static final String MESSAGE_SOURCE = "/WEB-INF/i18n/messages";
+    private static final String VIEWS = "/WEB-INF/views/";
+
+    private static final String RESOURCES_LOCATION = "/resources/";
+    private static final String RESOURCES_HANDLER = RESOURCES_LOCATION + "**";
+    
+    
+    @Bean(name = "messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(MESSAGE_SOURCE);
+        messageSource.setCacheSeconds(5);
+        return messageSource;
+    }
+    
 	@Bean
 	public ViewResolver thymeleafViewResolver(ITemplateEngine springTemplateEngine) {
 		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 		
-		viewResolver.setOrder(1);
 		viewResolver.setTemplateEngine(springTemplateEngine);
 		viewResolver.setCharacterEncoding("UTF-8");
 		viewResolver.setViewNames(new String[] {"*html"});
 		
 		return viewResolver;
 	}
+
+	//Sobre Beans: https://www.baeldung.com/spring-bean
 	
 	@Bean
-	public ITemplateEngine sprinhTemplateEngine(ITemplateResolver templateResolver) {
+	public SpringTemplateEngine sprinhTemplateEngine(ITemplateResolver templateResolver) {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		
-		templateEngine.setTemplateResolver(templateResolver);
-		templateEngine.setEnableSpringELCompiler(true);
-		
-		return templateEngine;
+        templateEngine.addTemplateResolver(new UrlTemplateResolver());
+        templateEngine.addTemplateResolver(templateResolver());
+        return templateEngine;
 	}
 	
 	@Bean
 	public ITemplateResolver templateResolver() {
-		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-		
-		templateResolver.setApplicationContext(applicationContext);
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		templateResolver.setPrefix("/WEB-INF/paginas");
-		templateResolver.setCharacterEncoding("UTF-8");
-		templateResolver.setCacheable(false);
-		
-		return templateResolver;
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+			
+   		resolver.setPrefix(VIEWS);
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+        return resolver;
 	}
+	
+	@Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource());
+        return validator;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(RESOURCES_HANDLER).addResourceLocations(RESOURCES_LOCATION);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
 }
